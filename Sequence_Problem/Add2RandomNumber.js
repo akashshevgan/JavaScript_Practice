77835b6daa309a2e5c2a2aa6d0ee4e5bfefa9ef6
akash <akash@MSI>
2021-06-07 14:25:59 +0530

    Javascript sequence problems

---
+++ Sequence_Problem/Add2RandomNumber.js
@@ -0,0 +1,5 @@
+var x = Math.floor(Math.random() * 6)+1;
+console.log("x is : " + x)
+var y = Math.floor(Math.random() * 6)+1;
+console.log("y is : " + y)
+console.log("Addition of two random dice numbers is : " + (x+y));
